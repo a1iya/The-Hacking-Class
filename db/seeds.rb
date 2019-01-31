3359595574955ca107b99de8ce6046201186c0da
a1iya <aliyusha.gizatullina@gmail.com>
2019-01-31 02:17:41 +0100

    version finale

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+	course = Course.create!(title: Faker::Hacker.abbreviation)
+end
+
+20.times do
+	student = Student.create!(name: Faker::Name.name, email: Faker::Internet.email, course_id: rand(1..10))
+end
